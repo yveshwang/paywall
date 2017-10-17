1853443736f7c368688d2c1b5b9bbe1ea131ffb5
Yves Hwang <yveshwang@gmail.com>
2017-10-17 14:20:59 +0200

    added aws-security-automation submodule, also tweaked CF to block calls from outside of denmark and norway

--- cloudfront.tf
+++ cloudfront.tf
@@ -9,7 +9,10 @@ resource "aws_cloudfront_distribution" "cloudfront_paywall" {
       origin_ssl_protocols = ["TLSv1.1"]
     }
   }
-
+  # TODO: ensure cloudformation is setup first. ths id is hardcoded for now.
+  # the quicker fix woudl be to use terraform to call cloudformation and obtain this id as output.
+  # the real fix would ge to not use cloudformation at all.
+  web_acl_id = "02e048b3-803f-4d16-87de-4a7a5f08be2e"
   enabled             = true
   is_ipv6_enabled     = true
   comment             = "paywall CDN - created via terraform."
@@ -36,8 +39,8 @@ resource "aws_cloudfront_distribution" "cloudfront_paywall" {
 
   restrictions {
     geo_restriction {
-      restriction_type = "none"
-      # locations        = ["US", "CA", "GB", "DE"]
+      restriction_type = "whitelist"
+      locations        = ["DK", "NO"]
     }
   }
 
