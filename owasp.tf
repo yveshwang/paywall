7f480c7b3821819412d4b7d32fbed63ac5266bf2
Yves Hwang <yveshwang@gmail.com>
2017-12-15 14:59:34 +0100

    rate limiting added

--- owasp.tf
+++ owasp.tf
@@ -1,8 +1,13 @@
 # yh
 # 14.12.2018
-
+# BUG: there is an issue with multiple rule in terraform not being synced.
+# see https://github.com/terraform-providers/terraform-provider-aws/issues/534
 resource "aws_waf_web_acl" "owasp_acl" {
-  depends_on  = ["aws_waf_ipset.blacklist_ipset", "aws_waf_rule.waf_blacklist_ip_rule"]
+  depends_on  = [ "aws_waf_ipset.blacklist_ipset",
+                  "aws_waf_rule.waf_blacklist_ip_rule",
+                  "aws_waf_ipset.private_ipset",
+                  "aws_waf_rate_based_rule.rate_limiting_rule"
+                ]
   name        = "owaspacl"
   metric_name = "owaspacl"
 
@@ -18,5 +23,13 @@ resource "aws_waf_web_acl" "owasp_acl" {
     priority = 1
     rule_id  = "${aws_waf_rule.waf_blacklist_ip_rule.id}"
     type     = "REGULAR"
+  }
+  rules {
+    action {
+      type = "BLOCK"
+    }
+    priority = 2
+    rule_id  = "${aws_waf_rate_based_rule.rate_limiting_rule.id}"
+    type     = "RATE_BASED"
   }
 }
--- wafrate.tf
+++ wafrate.tf
@@ -1,14 +1,16 @@
-resource "aws_waf_rate_based_rule" "wafrule" {
-  depends_on  = ["aws_waf_ipset.blacklist_ipset"]
+resource "aws_waf_rate_based_rule" "rate_limiting_rule" {
+  depends_on  = ["aws_waf_ipset.private_ipset"]
   name        = "ratelimiting"
   metric_name = "ratelimiting"
 
+  # max number of request as identified by IP in a given-5-min period
   rate_key = "IP"
   rate_limit = 2000
 
   predicates {
-    data_id = "${aws_waf_ipset.blacklist_ipset.id}"
-    negated = false
+    data_id = "${aws_waf_ipset.private_ipset.id}"
+    # apply to all public IPs except private reserved IPs.
+    negated = true
     type    = "IPMatch"
   }
 }
